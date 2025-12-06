37889b55c5d6c2f9a9806518490bf97274ff0cc5
mozart <204785202+devjuan8@users.noreply.github.com>
2025-12-06 15:19:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3001',
+    'https://calisinhuecosfront.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
